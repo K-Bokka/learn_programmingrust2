4141dfd0f56be2ecfd19fc8dbc48946e6409a8f5
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2022-10-29 14:44:50 +0900

    Fix mandelbrot single thread

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::str::FromStr;
 use num::Complex;
 use image::ColorType;
@@ -5,7 +6,23 @@ use image::png::PNGEncoder;
 use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPER_LEFT LOWER_RIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left conner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right conner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
@@ -92,20 +109,13 @@ fn test_pixel_to_point() {
         Complex { re: -0.5, im: -0.75 })
 }
 
-fn complex_square_add_loop(c: Complex<f64>) {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
-        z = z * z + c;
-    }
-}
-
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
             return Some(i);
         }
-        z * z + c;
+        z = z * z + c;
     }
     None
 }
