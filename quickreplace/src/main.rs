7d314f775c104e212bca5d214259f03b61b16d6b
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2022-11-06 19:20:58 +0900

    Add print_usage function

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,18 @@
+use text_colorizer::*;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn print_usage() {
+    eprintln!("{} - change occurrences of one string into another", "quickreplace".green());
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
