6e52c0941fa3f136c1d910b1c170fe3de4239227
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2022-12-14 12:21:12 +0900

    Add reference

---
+++ reference/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: Table) {
+    for (artist, works) in table {
+        println!("works by {}:", artist);
+        for work in works {
+            println!("    {}:", work);
+        }
+    }
+}
+
+fn main() {
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(), vec!["many madrigals".to_string(), "Tenebrae Responsoria".to_string()]);
+    table.insert("Caravaggio".to_string(), vec!["The Musicians".to_string()]);
+    table.insert("Suneo".to_string(), vec!["kono kuruma ha yonin nori nanda".to_string()]);
+
+    show(table);
+}
