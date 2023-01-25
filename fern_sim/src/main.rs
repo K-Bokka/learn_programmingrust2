428646215c0b5a111027501813913f8671a9a68f
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2023-01-25 13:16:17 +0900

    Fix fern sim

--- fern_sim/src/main.rs
+++ fern_sim/src/main.rs
@@ -1,9 +1,26 @@
-use crate::spores::produce_spore;
+struct Fern {
+    size: f64,
+    growth_rate: f64,
+}
 
-mod spores;
-mod plant_structures;
+impl Fern {
+    fn grow(&mut self) {
+        self.size *= 1.0 + self.growth_rate;
+    }
+}
+
+fn run_simiulation(fern: &mut Fern, days: usize) {
+    for _ in 0..days {
+        fern.grow();
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
-    produce_spore("test");
+    let mut fern = Fern {
+        size: 1.0,
+        growth_rate: 0.001,
+    };
+
+    run_simiulation(&mut fern, 1000);
+    println!("final fern size: {}", fern.size);
 }
