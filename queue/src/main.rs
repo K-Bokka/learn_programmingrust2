ccad3f25983da024d3c26fe804b2be1305ba5291
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2023-02-09 19:26:37 +0900

    Add Queue struct

--- queue/src/main.rs
+++ queue/src/main.rs
@@ -1,3 +1,36 @@
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>,
+}
+
+impl Queue {
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn shift(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+        self.older.pop()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut q = Queue { older: Vec::new(), younger: Vec::new() };
+
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.shift(), Some('0'));
+
+
+    q.push('∞');
+    assert_eq!(q.shift(), Some('1'));
+    assert_eq!(q.shift(), Some('∞'));
+    assert_eq!(q.shift(), None);
 }
