6be9bb2309d138b2ccc7502af768b6e7913dd4bd
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2023-02-16 19:40:00 +0900

    Add eval function to Polynomial

--- polynomial/src/main.rs
+++ polynomial/src/main.rs
@@ -1,3 +1,22 @@
+struct Polynomial<const N: usize> {
+    coefficient: [f64; N],
+}
+
+impl<const N: usize> Polynomial<N> {
+    fn new(coefficient: [f64; N]) -> Polynomial<N> {
+        Polynomial { coefficient }
+    }
+
+    fn eval(&self, x: f64) -> f64 {
+        let mut sum = 0.0;
+        for i in (0..N).rev() {
+            sum = self.coefficient[i] + x * sum;
+        }
+
+        sum
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
