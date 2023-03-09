ac90d6e9087532d0ddfc3d107dc1dfb73eb3554a
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2023-03-09 19:48:23 +0900

    fn say_hello

--- trait_gen/src/main.rs
+++ trait_gen/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::Write;
+
+fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
+    out.write_all(b"Hello world\n")?;
+    out.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut local_file = File::create("hello.txt").unwrap();
+    say_hello(&mut local_file).unwrap();
+
+    let mut bytes = vec![];
+    say_hello(&mut bytes).unwrap();
+    assert_eq!(bytes, b"Hello world\n");
 }
