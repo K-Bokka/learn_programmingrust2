4ccdce7958c339fddd86e6f532000caf3f9c3b20
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2023-03-16 19:45:35 +0900

    Add say_hello2

--- trait_gen/src/main.rs
+++ trait_gen/src/main.rs
@@ -6,11 +6,18 @@ fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
     out.flush()
 }
 
+fn say_hello2<W>(out: &mut W) -> std::io::Result<()>
+    where W: Write
+{
+    out.write_all(b"Hello world\n")?;
+    out.flush()
+}
+
 fn main() {
     let mut local_file = File::create("hello.txt").unwrap();
     say_hello(&mut local_file).unwrap();
 
     let mut bytes = vec![];
-    say_hello(&mut bytes).unwrap();
+    say_hello2(&mut bytes).unwrap();
     assert_eq!(bytes, b"Hello world\n");
 }
